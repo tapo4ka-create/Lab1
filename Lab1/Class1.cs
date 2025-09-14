e8411bd177b95a9009e4f5291e755c12705faa14
Ilya <ilya.verevkin.2018@mail.ru>
2025-09-14 14:19:22 +0700

    сущность биг боб

--- Lab1/Class1.cs
+++ Lab1/Class1.cs
@@ -6,12 +6,11 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public int Year { get; set; }
-
-    }
+        public class Teacher
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Subject { get; set; }
+            public int Experience { get; set; }
+        }
 }
